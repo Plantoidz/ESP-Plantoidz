c503a5ed66512b831b9bd0cd2d7fcd2794c472e2
mart1ver <mart1ver@gmail.com>
2024-05-26 17:27:50 +0200

    préparation à la customisation de wifimanager

--- Plantoid-node/wmFunctions.h
+++ Plantoid-node/wmFunctions.h
@@ -42,3 +42,106 @@ void saveParamCallback() {
   if (serialDebug) Serial.println("[CALLBACK] saveParamCallback fired");
   if (serialDebug) Serial.println("PARAM customfieldid = " + getParam("customfieldid"));
 }
+
+
+void setupWm() {
+  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
+  delay(3000);
+  Serial.println("\n Starting");
+  // wm.resetSettings(); // wipe settings
+
+  if (wm_nonblocking) wm.setConfigPortalBlocking(false);
+
+  // add a custom input field
+  int customFieldLength = 40;
+
+
+  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\"");
+
+  // test custom html input type(checkbox)
+  // new (&custom_field) WiFiManagerParameter("customfieldid", "Custom Field Label", "Custom Field Value", customFieldLength,"placeholder=\"Custom Field Placeholder\" type=\"checkbox\""); // custom html type
+
+  // test custom html(radio)
+  const char* custom_radio_str = "<br/><label for='customfieldid'>Custom Field Label</label><input type='radio' name='customfieldid' value='1' checked> One<br><input type='radio' name='customfieldid' value='2'> Two<br><input type='radio' name='customfieldid' value='3'> Three";
+  new (&custom_field) WiFiManagerParameter(custom_radio_str);  // custom html input
+
+  wm.addParameter(&custom_field);
+  wm.setSaveParamsCallback(saveParamCallback);
+
+  // custom menu via array or vector
+  //
+  // menu tokens, "wifi","wifinoscan","info","param","close","sep","erase","restart","exit" (sep is seperator) (if param is in menu, params will not show up in wifi page!)
+  // const char* menu[] = {"wifi","info","param","sep","restart","exit"};
+  // wm.setMenu(menu,6);
+  std::vector<const char*> menu = { "wifi", "info", "param", "sep", "restart", "exit" };
+  wm.setMenu(menu);
+
+  // set dark theme
+  wm.setClass("invert");
+
+
+  //set static ip
+  // wm.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0)); // set static ip,gw,sn
+  // wm.setShowStaticFields(true); // force show static ip fields
+  // wm.setShowDnsFields(true);    // force show dns field always
+
+  // wm.setConnectTimeout(20); // how long to try to connect for before continuing
+  wm.setConfigPortalTimeout(30);  // auto close configportal after n seconds
+  // wm.setCaptivePortalEnable(false); // disable captive portal redirection
+  // wm.setAPClientCheck(true); // avoid timeout if client connected to softap
+
+  // wifi scan settings
+  // wm.setRemoveDuplicateAPs(false); // do not remove duplicate ap names (true)
+  // wm.setMinimumSignalQuality(20);  // set min RSSI (percentage) to show in scans, null = 8%
+  // wm.setShowInfoErase(false);      // do not show erase button on info page
+  // wm.setScanDispPerc(true);       // show RSSI as percentage not graph icons
+
+  // wm.setBreakAfterConfig(true);   // always exit configportal even if wifi save fails
+
+  bool res;
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  res = wm.autoConnect("theplant");  // password protected ap
+
+  if (!res) {
+    Serial.println("Failed to connect or hit timeout");
+
+  } else {
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+  }
+}
+
+/*///////////////////////////////////////// config de wifimanager
+
+  if (wm_nonblocking) wm.setConfigPortalBlocking(false);
+  // add a custom input field
+  int customFieldLength = 40;
+  // test custom html(radio)
+  const char* custom_radio_str = "<br/><label for='customfieldid'>Custom Field Label</label><input type='radio' name='customfieldid' value='1' checked> One<br><input type='radio' name='customfieldid' value='2'> Two<br><input type='radio' name='customfieldid' value='3'> Three";
+  new (&custom_field) WiFiManagerParameter(custom_radio_str);  // custom html input
+  wm.addParameter(&custom_field);
+  wm.setSaveParamsCallback(saveParamCallback);
+  std::vector<const char*> menu = { "wifi", "info", "param", "sep", "restart", "exit" };
+  wm.setMenu(menu);
+  // set dark theme
+  wm.setClass("invert");
+  bool res;
+
+  Serial.println("helooo");
+  res = wm.autoConnect("unconfiguredPlantoid", apPassword);  // password protected ap
+
+  Serial.println("hello i'm here");
+
+  if (!res) {
+    if (serialDebug) Serial.println("Failed to connect or hit timeout");
+    // ESP.restart();
+  } else {
+    //if you get here you have connected to the WiFi
+    if (serialDebug) Serial.println("Pantoid connected... by wifi :)");
+  }
+  */
+///////////////////////////////////////////////////////////////////////////
+
+
+
